8ae1fb3fa6dbd9bc1017d0d27c61569b69471061
Lilian2588 <113914372+Lilian2588@users.noreply.github.com>
2026-02-14 23:37:34 +0100

    Remove temporary logs for Supabase credentials
    Removed temporary logging of Supabase URL and key.

--- quiz-app/src/supabaseClient.js
+++ quiz-app/src/supabaseClient.js
@@ -3,8 +3,4 @@ import { createClient } from '@supabase/supabase-js'
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
 const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY
 
-// AJOUTE CES LOGS TEMPORAIRES
-console.log("URL Supabase:", supabaseUrl)
-console.log("Key Supabase:", supabaseAnonKey)
-
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
